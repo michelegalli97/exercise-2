635e955a06d9d6ea1bc98adbe92304d732163581
michelegalli97 <mikigalli@gmail.com>
2017-11-29 20:09:15 +0100

    Update particle_main_verlet.py

--- particle_main_verlet.py
+++ particle_main_verlet.py
@@ -1,15 +1,11 @@
 """
 CMod Ex3: velocity Verlet time integration of
-a particle moving in a double well potential.
-
-Produces plots of the position of the particle
-and its energy, both as function of time. Also
+2 particles moving in a Morse pair potential.
+Produces plots of the relative distance between the particles
+and the total energy, both as function of time. Also
 saves both to file.
-
-The potential is V(x) = a*x^4 - b*x^2, where
-a and b are hard-coded in the main() method
-and passed to the functions that
-calculate force and potential energy.
+The Morse potential needs parameters Re, De, alpha. these are taken
+from the input file that sets up initial conditions. (should be included by user)
 """
 
 import sys
@@ -20,26 +16,29 @@ from Particle3D import Particle3D
 
 def force_dw(particle1, particle2, alpha, De, Re):
     """
-    Method to return the force on a particle
-    in a double well potential.
-    :return: force acting on particle as Numpy array
+    Method to define force on a particle.
+
+    :param particle1, particle2 : Particle3D instances
+    :param alpha, De, Re : floats (potential parameters taken from input file)
+    :return: force acting on (by convention) particle 1 as array (floats)
     """
-    r12 = Particle3D.relative_pos(particle1,particle2)
+    r12 = Particle3D.relative_pos(particle1,particle2)  # define useful quantities for force
     modr12 = np.linalg.norm(r12)
     r12hat = (1/modr12)*r12
-    force = -2*alpha*De*(1-math.exp(-alpha*(modr12-Re)))*math.exp(-alpha*(modr12-Re))*r12hat
+    force = -2*alpha*De*(1-math.exp(-alpha*(modr12-Re)))*math.exp(-alpha*(modr12-Re))*r12hat #define force
     return force
 
 def pot_energy_dw(particle1, particle2, alpha, De, Re):
     """
-    Method to return potential energy 
-    of particle in double-well potential
-    :param a: parameter a from potential
-    :param b: parameter b from potential
-    :return: potential energy of particle as float
+    Method to define total potential energy
+    of a pair of particles.
+
+    :param particle1, particle2 : Particle3D instances
+    :param alpha, De, Re : floats
+    :return: total potential energy of as a float
     """
-    modr12 = np.linalg.norm(Particle3D.relative_pos(particle1, particle2))
-    potential = De*((1-math.exp(-alpha*(modr12-Re)))**2-1)
+    modr12 = np.linalg.norm(Particle3D.relative_pos(particle1, particle2)) # define ||r12||
+    potential = De*((1-math.exp(-alpha*(modr12-Re)))**2-1)      # define potential
     return potential
 
 # Begin main code
@@ -57,22 +56,23 @@ def main():
     # Open output file
     outfile = open(outfile_name, "w")
     infile = open(infile_name, "r")
+    lines = infile.readlines()     # initialise list of lists, like in init_from_file()
+    choose = lines[1].split(",")   #split line "1" to get potential parameters
 
     # Set up simulation parameters
-    dt = 0.107
-    numstep = 1000
+    dt = 0.107     ###### dt is here ######
+    numstep = 500  ###### numstep is here ######
     time = 0.0
-    alpha = float(infile.readline())
-    De =  float(infile.readline())
-    Re = float(infile.readline())
+    alpha = float(choose[2])
+    De =  float(choose[1])  
+    Re = float(choose[0])    
     infile.close()
 
-    # Set up particle initial conditions:
-    #  position x0 = 0.0
-    #  velocity v0 = 1.0
-    #  mass      m = 1.0
-    p1 = Particle3D.init_from_file(infile_name)
-    p2 = Particle3D.init_from_file("newfile.txt")
+
+    p1 = Particle3D.init_from_file(infile_name,1)
+    p2 = Particle3D.init_from_file(infile_name,2)
+    #######print("p1 position is "+ str(p1.position)) SOME TESTS CAN DELETE #########
+    ####### print("De is" + str(De)) SOME TESTS CAN DELETE ########
 
     # Write out initial conditions
     energy = p1.kinetic_energy() + p2.kinetic_energy() + pot_energy_dw(p1, p2, alpha, De, Re)
@@ -80,7 +80,7 @@ def main():
     outfile.write("{0:f} {1:f} {2:12.8f}\n".format(time,np.linalg.norm(Particle3D.relative_pos(p1,p2)),energy))
 
     # Get initial force
-    force = force_dw(p1, p2, alpha, De, Re)
+    force = force_dw(p1, p2, alpha, De, Re) # force on p1
 
     # Initialise data lists for plotting later
     time_list = [time]
@@ -112,7 +112,7 @@ def main():
 
         # Append information to data lists
         time_list.append(time)
-        relpos_list.append(np.linalg.norm(Particle3D.relative_pos(p1,p2)))
+        relpos_list.append(np.linalg.norm(Particle3D.relative_pos(p1,p2))) #list of scalar distances (for plotting ) not positions 
         energy_list.append(energy)
 
     # Post-simulation:
@@ -131,16 +131,16 @@ def main():
     outfile.close()
 
    # Plot particle trajectory to screen
-    pyplot.title('Velocity Verlet: relative position vs time')
-    pyplot.xlabel('Time')
-    pyplot.ylabel('Relative Position')
+    pyplot.title('Velocity Verlet: relative distance vs time')
+    pyplot.xlabel('Time (natural unit ~ 10.18e-15 s)')
+    pyplot.ylabel('Relative Distance (Angstrom)')
     pyplot.plot(time_list, relpos_list)
     pyplot.show()
 
     # Plot particle energy to screen
     pyplot.title('Velocity Verlet: total energy vs time')
-    pyplot.xlabel('Time')
-    pyplot.ylabel('Energy')
+    pyplot.xlabel('Time (natural unit ~ 10.18e-15 s)')
+    pyplot.ylabel('Energy (eV)')
     pyplot.plot(time_list, energy_list)
     pyplot.show()
 
